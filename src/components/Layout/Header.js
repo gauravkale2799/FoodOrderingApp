d6c33b767a5cf2c999a211aadcec72605a98edd5
gauravkale2799 <gauravkale2799@gmail.com>
2023-04-22 17:21:05 +0200

    adding button

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+import HeadeCartButton from "./HeaderCartButtorn";
+
+import mealsImage from '../assets/meals.jpeg'
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeadeCartButton/>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Your good food"></img>
+        {/* <img src="https://cdn.ning.com/wp-content/uploads/2019/05/create-food-website.jpg" alt="A table ful of delisious food" /> */}
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
