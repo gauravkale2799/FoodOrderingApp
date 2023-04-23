b325c5f4ba0793c6d5bdeb1ce6356b6f9a556c0d
gauravkale2799 <gauravkale2799@gmail.com>
2023-04-23 16:34:47 +0200

    adding Card Button

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,32 @@
+import classes from './Cart.module.css'
+
+
+
+
+
+const Cart = (props) =>{
+    const cartItems = <ul className={classes['cart-items']}>
+        {[{id: 'c1', name: 'sushi', amount:2 , price :23.23},
+    ].map((items) => <li>{item.name}</li>)}
+    </ul>;
+    return(
+        <div>
+            {cartItems}
+            <div className={clases.total}>
+                <span>Total Amount</span>
+                <span>35.62</span>
+            </div>
+            <div className={classes.action}>
+                <button className={classes['button--alt']}>Close</button>
+                <button className={classes.button}>Order</button>
+
+            </div>
+        </div>
+    )
+
+};
+
+
+
+
+export default Cart;
